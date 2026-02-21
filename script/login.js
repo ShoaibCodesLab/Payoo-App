3568da502c619f258f09a2331f9b6eeb7d5c437e
Shoaib-365 <shoaibsiam4@gmail.com>
2026-02-21 23:17:03 +0600

    first commit

---
+++ script/login.js
@@ -0,0 +1,6 @@
+console.log("Login functionality comming");
+
+document.getElementById("login-btn").addEventListener("click", function () {
+    // 1- get the mobile number input
+    constu nmberInput = 
+})
