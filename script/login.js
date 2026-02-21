acffc665f67ef632d2092205ae9f2a4c804df845
Shoaib-365 <shoaibsiam4@gmail.com>
2026-02-21 23:44:05 +0600

    login functionality done

--- script/login.js
+++ script/login.js
@@ -1,6 +1,29 @@
 console.log("Login functionality comming");
 
 document.getElementById("login-btn").addEventListener("click", function () {
+    
     // 1- get the mobile number input
-    constu nmberInput = 
+    const numberInput = document.getElementById("input-number");
+    const contactNumber = numberInput.value;
+    console.log(contactNumber);
+
+    // 2- get the pin input
+    const inputPin = document.getElementById("input-pin");
+    const pin = inputPin.value;
+    console.log(pin);
+
+    // 3- match pin & mobile number
+    if(contactNumber=="01748141668" && pin=="1234"){
+
+        // 3-1 true:::>> alert> homepage
+        alert("Login Success");
+
+        // window.location.replace("/home.html");
+        window.location.assign("/home.html");
+    }else{
+        // 3-2 false:::>> alert> return
+        alert("Login Failed");
+        return;
+    }
+    
 })
